c79a9a5366b9c1ecda87c7ff4381bf98f11ff963
Jimil Patel <jimil.patel@jimilpatel-ltm1.internal.salesforce.com>
2020-01-29 15:32:44 -0800

    deploying to heroku

--- server.js
+++ server.js
@@ -36,7 +36,7 @@ if(process.env.NODE_ENV === 'production'){
   //set static folder
   app.use(express.static('client/build'));
   app.get('*', (req,res) => {
-    res.sendFile(path.resolve(__dirname, 'cleint', 'build','index.html'));
+    res.sendFile(path.resolve(__dirname, 'client', 'build','index.html'));
   });
 }
 
